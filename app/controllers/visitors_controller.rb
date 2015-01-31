d7777d675c9f306a07bfcef8ec88993b02e7fba8
Mark Booth <mrbooth@gmail.com>
2015-01-31 16:29:47 +0000

    mailing list using MailChimp

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,7 +1,25 @@
 class VisitorsController < ApplicationController
 
   def new
-    @owner = Owner.new
+    @visitor = Visitor.new
+  end
+
+  def create
+    @visitor = Visitor.new(secure_params)
+    if @visitor.valid?
+      @visitor.subscribe
+      flash[:notice] = "Signed up #{@visitor.email}."
+      redirect_to root_path
+    else
+      flash[:alert] = "Sorry, there was an issue you signing you up. PLease try again"
+      render :new
+    end
+  end
+
+  private
+
+  def secure_params
+    params.require(:visitor).permit(:email)
   end
 
 end
